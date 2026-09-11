1fafaff6b93cf5b5c3770fd70016c567a7eedd27
aj0khi <khidri.ajmal@gmail.com>
2026-09-11 16:25:48 -0600

    new code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import math
+import sys
 from pathlib import Path
 from utils import celsius_to_fahrenheit, describe_temperature, average
 # alternative
 # from utils import *
 # from utils import average
 # import utils but then you have to specify utils.average
-
+def main():
+    print("I am in main!")
 MAX = 600
 
 # in java, they call functions - methods
@@ -16,3 +18,6 @@ readings: list[float] = [-12.5, -8.0, 0.0, 4.5]
 
 print(f"Average: {average(readings):.1f} C")
 
+if __name__ == "__main__":
+    main()  
+    sys.exit(0)
